386ee77ab80f13acd2a71a141865479878363a18
Vennila200405 <vennilas2004@gmail.com>
2025-08-04 15:53:03 +0530

    Create testbench.v

---
+++ Single port RAM with synchronous read/testbench.v
@@ -0,0 +1,70 @@
+module tb_raminfr;
+
+    
+    reg clk;
+    reg we;
+    reg [4:0] a;
+    reg [3:0] di;
+    wire [3:0] do;
+
+     
+    raminfr uut (
+        .clk(clk),
+        .we(we),
+        .a(a),
+        .di(di),
+        .do(do)
+    );
+
+   
+    always #5 clk = ~clk;
+
+    initial begin
+      
+        clk = 0;
+        we = 0;
+        a = 0;
+        di = 0;
+
+      
+        #10;
+
+     
+        @(negedge clk);
+        we = 1;
+        a = 5'd5;
+        di = 4'b1010;
+
+       
+        @(negedge clk);
+        a = 5'd10;
+        di = 4'b1100;
+
+      
+        @(negedge clk);
+        we = 0;
+
+     
+        @(negedge clk);
+        a = 5'd5;
+
+        @(negedge clk);
+        $display("Read do from address 5 = %b", do);
+
+     
+        @(negedge clk);
+        a = 5'd10;
+
+        @(negedge clk);
+        $display("Read do from address 10 = %b", do);
+
+     
+        @(negedge clk);
+        a = 5'd7;
+
+        @(negedge clk);
+        $display("Read do from address 7 = %b", do);
+
+        $finish;
+    end
+          endmodule
