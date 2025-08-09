86ccebc25b720fd0468a90015fdaca9db85b83fb
Vennila200405 <vennilas2004@gmail.com>
2025-08-09 23:17:22 +0530

    Update Parity_generator.v

--- Parity_generator/Parity_generator.v
+++ Parity_generator/Parity_generator.v
@@ -1,15 +1,12 @@
 module even_parity_generator (
-    input  [3:0] data_in,
-    output       parity_bit
+  input  [3:0] data_in,
+  output parity_bit
 );
-  
 endmodule
 
-
 module odd_parity_generator (
-    input  [3:0] data_in,
-    output       parity_bit
+  input  [3:0] data_in,
+  output parity_bit
 );
-    
-    assign parity_bit = ~(^data_in);
+  assign parity_bit = ~(^data_in);
 endmodule
