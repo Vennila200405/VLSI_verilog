1e7cd2bd6b061be9cc250a4db382924555d32751
Vennila200405 <vennilas2004@gmail.com>
2025-08-15 22:42:09 +0530

    Update 2_input_priority_arbiter.v

--- 2_input_priority arbiter/2_input_priority_arbiter.v
+++ 2_input_priority arbiter/2_input_priority_arbiter.v
@@ -1,14 +1,8 @@
 module priority_arbiter_dataflow (
-    input wire req0,
-    input wire req1,
-    output wire grant0,
-    output wire grant1
-);
-
-
-assign grant0 = req0;
-
-
-assign grant1 = ~req0 & req1;
-
+  input wire req0,
+  input wire req1,
+  output wire grant0,
+  output wire grant1);
+  assign grant0 = req0;
+  assign grant1 = ~req0 & req1;
 endmodule
